9a35995337830c5113d8b1e8f8b55816905ba908
Yahor_Kavaliou <yahor.kavaliou@thomsonreuters.com>
2016-10-14 17:03:57 +0300

    Egor - GUI update

--- src/main/java/parser/MainForm.java
+++ src/main/java/parser/MainForm.java
@@ -6,42 +6,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.*;
-import java.util.List;
 
-public class Form extends JFrame implements ActionListener {
-    JTextArea textArea;
-    JButton buttonOpenFile;
-    JButton buttonParseFile;
+public class MainForm extends JFrame implements ActionListener {
+    private JButton buttonOpenFile;
+    private JButton buttonParseFile;
+    private JTextArea textArea;
+    private JPanel myFrame;
+
     private String pathToFile = "";
 
-    public static void createAndShowGUI() {
-        Form myFrame = new Form();
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.prepareUI();
-        myFrame.pack();
-        myFrame.setVisible(true);
-    }
-
-    private void prepareUI(){
-        textArea = new JTextArea();
-        textArea.setEditable(false);
-        JScrollPane panel = new JScrollPane(textArea);
-        panel.setPreferredSize(new Dimension(200, 50));
-        buttonParseFile = new JButton("Parse file");
-        buttonOpenFile = new JButton("Open File");
-        buttonOpenFile.addActionListener(this);
-        buttonParseFile.addActionListener(this);
-        buttonParseFile.setEnabled(false);
-        getContentPane().add(panel, BorderLayout.CENTER);
-        getContentPane().add(buttonOpenFile, BorderLayout.PAGE_START);
-        getContentPane().add(buttonParseFile, BorderLayout.PAGE_END);
+    public static void createAndShowGUI(){
+        MainForm mainForm = new MainForm();
+        JFrame frame = new JFrame("MainForm");
+        frame.setContentPane(mainForm.myFrame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainForm.prepareUI();
+        frame.pack();
+        frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == buttonOpenFile){
             final JFileChooser jFileChooser = new JFileChooser();
-            int returnVal = jFileChooser.showOpenDialog(Form.this);
+            int returnVal = jFileChooser.showOpenDialog(MainForm.this);
             if(returnVal == JFileChooser.APPROVE_OPTION){
                 pathToFile = jFileChooser.getSelectedFile().getAbsolutePath();
                 textArea.setText("Selected file: " + pathToFile);
@@ -56,7 +44,7 @@ public class Form extends JFrame implements ActionListener {
         }
 
         if(e.getSource() == buttonParseFile){
-            List<Scenario> scenarios = new ArrayList<>();
+            java.util.List<Scenario> scenarios = new ArrayList<>();
             try {
                 scenarios = HTMLReader.readFile(pathToFile);
             }
@@ -65,5 +53,12 @@ public class Form extends JFrame implements ActionListener {
             }
             CSVWriter.writeCsvFile("4.csv", scenarios);
         }
+    }
+
+    private void prepareUI(){
+        textArea.setEditable(false);
+        buttonOpenFile.addActionListener(this);
+        buttonParseFile.addActionListener(this);
+        buttonParseFile.setEnabled(false);
     }
 }
